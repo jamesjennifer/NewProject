b9134f4f7374d326ae45e32891259ec5bf1eef00
jamesjenni <mercyjames9095@gmail.com>
2021-05-10 18:23:59 +0530

    removed the space

--- src/org/bash/ProjectDay1.java
+++ src/org/bash/ProjectDay1.java
@@ -3,10 +3,6 @@ package org.bash;
 public class ProjectDay1 {
 public static void main(String[] args) {
 	String S="Hai this is my new Practise";
-
-	
-
-
 	System.out.println(S);	
 	String f="james";
 			System.out.println(f);
